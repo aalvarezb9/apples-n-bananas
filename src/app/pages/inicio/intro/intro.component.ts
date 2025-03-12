b625da12ee36bcbb2102af33e45eea4ec19c9c3f
Angel Alvarez <angel@escala.com>
2025-03-12 07:14:48 -0600

    adding new images

--- src/app/pages/inicio/intro/intro.component.ts
+++ src/app/pages/inicio/intro/intro.component.ts
@@ -6,6 +6,7 @@ import { IsMobileService } from '../../../services/is-mobile.service';
 import { ImageLoaderDirective } from '../../../directives/image-loader.directive';
 import { ImagesLoadedService } from '../../../services/image-loader.service';
 import { LoaderComponent } from '../../../components/loader/loader.component';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-intro',
@@ -28,13 +29,16 @@ export class IntroComponent implements OnInit {
   private readonly textAnimationService = inject(TextAnimationService);
   private readonly isMobileService = inject(IsMobileService);
   private readonly imagesLoadedService = inject(ImagesLoadedService);
+  logoSrc = new BehaviorSubject<string | null>(null);
   modalCerrada = output<void>();
   isLoadingImages = true;
+  BASE_LOGOS_SRC = '../../../../assets/logos';
 
   ngOnInit(): void {
     this.empezarAnimacion();
     this.isMobileService.isMobileView$.subscribe((isMobileView) => {
       this.isMobileView = isMobileView;
+      this.logoSrc.next(this.isMobileView ? `${this.BASE_LOGOS_SRC}/logo_mobile.png` : `${this.BASE_LOGOS_SRC}/logo_pc.png`);
     });
 
     this.imagesLoadedService.allImagesLoaded$.subscribe((allLoaded) => {
